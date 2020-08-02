2b5dfd906516fae0c164bbc1655481e33047146f
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-08-02 22:51:26 +0530

    UI Developed with card added header footer and main

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import clasess from './index.module.css';
 import { Card, Button } from "react-bootstrap";
-import { Paper } from "@material-ui/core"
+import { Paper } from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 const items = (props) => {
     return (
 
-        <Card border="secondary" style={{ width: '18rem' }} >
-            <Paper  style={{  padding: '10px' }} elevation={3}>
-                <Card.Img variant="top" src="https://staticimg.titan.co.in/Tanishq/Catalog/502998SBHAAA04_2.jpg" style={{ width: '180px' }} />
+        <Paper elevation={12} style={{ display: 'inline-block', margin: '1rem' }}>
+            <Card border="secondary" style={{ width: '12rem', margin: '10px' }} >
+                <Paper style={{ padding: '10px' }} elevation={3}>
+                    <Card.Img variant="top" src="https://staticimg.titan.co.in/Tanishq/Catalog/502998SBHAAA04_2.jpg" style={{ width: '180px' }} />
+                </Paper>
                 <Card.Body>
                     <Card.Title>{props.title}</Card.Title>
-                    <Card.Text>
-                       <Paper>
-                           
-                       </Paper>
+                    <Card.Text >
+                        <Paper elevation={5} style={{ padding: '5px', margin: '1rem' }}>{props.brand}</Paper>
+                        <Paper elevation={5} style={{ padding: '5px', textAlign: 'left' }}>
+                            <div style={{ fontSize: "10px" }}>
+                                Price: {'\u20B9'} {props.price ? props.price : "NA"}<br />
+                                Category: {props.category ? props.category : "NA"}<br />
+                                Collection: {props.collection ? props.collection : "NA"}<br />
+                            </div>
+                        </Paper>
                     </Card.Text>
-                    <Button variant="primary" style={{  backgroundColor: 'blue' , color: 'white'}}>Buy Now</Button>
+                    <Paper elevation={5} style={{ background: 'linear-gradient(to right, #2193b0, #6dd5ed)', color: 'white', width: "40%", display: "inline-block", padding: "2px" }}>Buy Now</Paper>
                 </Card.Body>
-            </Paper>
-        </Card>
+            </Card>
+        </Paper>
 
     );
 }
--- src/container/Main.js
+++ src/container/Main.js
@@ -1,19 +1,67 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Card from "../components/Card/index";
-class Main extends Component
-{
-    constructor()
-    {
+import { Paper } from "@material-ui/core";
+import { readString, readRemoteFile } from 'react-papaparse'
+class Main extends Component {
+    constructor() {
         super();
+        this.state = {
+            data: [],
+            pageNumber: 1,
+            pageCount: 30,
+            lastIndex: 2,
+        }
     }
-    render()
+    componentDidMount() {
+        readRemoteFile('/Assets/data.csv', {
+            download: true,
+            delimiter: ",",
+            complete: (results) => {
+                console.log(results);
+                this.setState({ data: results.data });
+            }
+        })
+    }
+    getData = ()=>
     {
+        let count = this.state.lastIndex;
+        let data = this.state.data.map(item => {
+            if (count < this.state.pageCount) {
+                count++;
+                return (
+                    <Card key={item[0]} brand={item[48]} price={item[46]} category={item[36].split(' ')[0]} collection={item[8]}></Card>
+                )
+            }
+        }
+        )
+        return data;
+    }
+    nextHandler = () =>
+    {
+
+    }
+
+    prevHandler = () =>
+    {
+        
+    }
+
+    render() {
+        let data = this.getData();
         return (
             <React.Fragment>
-                <Card></Card>
-            </React.Fragment>
+                <header>
+                    <Paper elevation={12}><img src="https://miro.medium.com/max/600/1*jQW88p6H4G1Djbnx4wiuwQ.jpeg" width="99%" padding="1%"></img></Paper>
+                </header>
+                <main>
+                    {data}
+                </main>
+                <footer>
+                    <Paper onClick={this.nextHandler} style={{ display: "inline-block", width: "10%", margin: "0.5rem", padding: "0.5rem", background: 'linear-gradient(to right, #1fa2ff, #12d8fa, #a6ffcb)',marginTop:"2%" }}>Next</Paper>
+                    <Paper onClick={this.prevHandler} style={{ display: "inline-block", width: "10%", margin: "0.5rem", padding: "0.5rem", background: 'linear-gradient(to right, #a6ffcb, #12d8fa, #1fa2ff)',marginTop:"2%" }}>Prev</Paper>
+                    <Paper elevation={12} style={{ background: 'linear-gradient(to right, #00b4db, #0083b0)', marginTop: "5%" }}>Designed by Shahid Dhariwala</Paper>
+                </footer>
+            </React.Fragment >
         );
     }
 }
-
-export default Main;
